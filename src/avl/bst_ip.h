f0c00784724e6a7bfd0f32fb5a34773f200bb5a1
SDC-M <182355397+SDC-M@users.noreply.github.com>
2025-03-24 16:38:22 +0100

    structure of project

---
+++ src/avl/bst_ip.h
@@ -0,0 +1,32 @@
+//  bst_ip.h : précisions sur l'implantation du module bst.
+
+//  bst_empty : temps constant ; espace constant
+
+//  bst_dispose : temps linéaire en la taille de l'arbre ; espace linéaire en la
+//    hauteur de l'arbre
+
+//  bst_add_endofpath : temps au plus linéaire en la hauteur de l'arbre;
+//    espace constant
+
+//  bst_remove_climbup_left : temps au plus linéaire en la hauteur de l'arbre;
+//    espace constant
+
+//  bst_search : temps au plus linéaire en la hauteur de l'arbre;
+//    espace constant
+
+//  bst_size : temps constant ; espace constant
+
+//  bst_height : temps constant ; espace constant
+
+//  bst_distance : temps au plus linéaire en la hauteur de l'arbre ;
+//    espace constant
+
+//  bst_number : temps au plus linéaire en la hauteur de l'arbre ; espace
+//    constant
+
+//  bst_rank : temps au plus linéaire en la hauteur de l'arbre ; espace
+//    constant
+
+//  bst_repr_graphic : temps quadratique ; espace quadratique
+
+//  bst_dft_infix_apply_context : temps ... ; espace ...
