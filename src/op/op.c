f4bc621cf29eb46a13fcc7d95e1c521ce9da7e5f
P4R4D0X15 <nicolas@warriordudimanche.net>
2025-03-27 11:57:00 +0100

    op.c

--- src/op/op.c
+++ src/op/op.c
@@ -1,5 +1,26 @@
 #include <stdlib.h>
+#include "bst.h"
 
-int mainj() {
+int help() {
+  fprintf(stdout, "Calcul distance de Jaccard\n");
+  fprintf(stdout, "\t -? : Affiche ce menu d'aide\n");
+  fprintf(stdout,
+      "\t -g : Affiche le graphe d'appartenance des mots "
+      "dans les fichiers passés sur la ligne de commande\n");
+  fprintf(stdout, "\t -i VALUE : permet de fixer la longueur maximale "
+      "des mots à VALUE\n");
+  fprintf(stdout, "\t -p : Considère les caractères de ponctuation "
+      "comme des caractères d'espacement");
+  fprintf(stdout,
+      "\t -- : Indique que l'argument qui suit doit être "
+      "considéré comme un fichier\n");
   return 0;
 }
+
+int graph_belonging(/*Paramètre WIP*/) {
+
+}
+
+int set_maxlength()
+
+// rfree : libère la mémoire pointé par ptr et renvoie 0.
