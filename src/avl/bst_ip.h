aefecbc81f9628507460d804e968c2133415b3f2
P4R4D0X15 <nicolas@warriordudimanche.net>
2025-04-26 12:58:25 +0200

    precision && little clean up

--- src/avl/bst_ip.h
+++ src/avl/bst_ip.h
@@ -2,14 +2,14 @@
 
 //  bst_empty : temps constant ; espace constant
 
-//  bst_dispose : temps linéaire en la taille de l'arbre ; espace linéaire en la
-//    hauteur de l'arbre
+//  bst_dispose : temps quadratique en la taille de l'arbre ; espace linéaire
+//    en la hauteur de l'arbre
 
 //  bst_add_endofpath : temps au plus linéaire en la hauteur de l'arbre;
-//    espace constant
+//    espace au plus linéaire en la hauteur de l'arbre
 
 //  bst_remove_climbup_left : temps au plus linéaire en la hauteur de l'arbre;
-//    espace constant
+//    espace au plus linéaire en la hauteur de l'arbre
 
 //  bst_search : temps au plus linéaire en la hauteur de l'arbre;
 //    espace constant
@@ -19,14 +19,15 @@
 //  bst_height : temps constant ; espace constant
 
 //  bst_distance : temps au plus linéaire en la hauteur de l'arbre ;
-//    espace constant
+//    espace au plus linéaire en la hauteur de l'arbre
 
 //  bst_number : temps au plus linéaire en la hauteur de l'arbre ; espace
-//    constant
+//    au plus linéaire en la hauteur de l'arbre
 
 //  bst_rank : temps au plus linéaire en la hauteur de l'arbre ; espace
-//    constant
+//    au plus linéaire en la hauteur de l'arbre
 
 //  bst_repr_graphic : temps quadratique ; espace quadratique
 
-//  bst_dft_infix_apply_context : temps ... ; espace ...
+//  bst_dft_infix_apply_context : temps quadratique en la taille de l'arbre ;
+//     espace linéaire en la hauteur de l'arbre
