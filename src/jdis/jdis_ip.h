c1b21d98c9c8858160055923bd2f23f8d5c14ce1
P4R4D0X15 <nicolas@warriordudimanche.net>
2025-04-26 13:12:41 +0200

    add jdis_ip.h

---
+++ src/jdis/jdis_ip.h
@@ -0,0 +1,12 @@
+//  jdis_ip.h : précisions sur l'implantation du module jdis.
+
+// is_in : temps au plus linéaire en la hauteur de l'arbre ; espaces :
+//      au plus linéaire en la hauteur de l'arbre
+
+// card_intersection : temps quadratique en la taille de l'arbre ; espaces :
+//    linéaire en la hauteur de l'arbre
+
+// jdis : temps constant ; espaces constant
+
+// file_to_bst : temps linéaire en la taille du fichier ; espaces au plus
+//    linéaire en la taille du fichier
